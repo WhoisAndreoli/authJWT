3d407a0288cc0bda21f1e29c67b59e84f53c6f29
Andre <andre_oliiveira97@hotmail.com>
2022-07-17 01:28:05 -0300

    add appUser controller

---
+++ src/main/java/com/example/authjwt/controllers/AppUserController.java
@@ -0,0 +1,25 @@
+package com.example.authjwt.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.authjwt.dtos.AppUserRequest;
+import com.example.authjwt.dtos.AppUserResponse;
+import com.example.authjwt.services.AppUserServiceImpl;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class AppUserController {
+
+  private AppUserServiceImpl appUserServiceImpl;
+
+  public ResponseEntity<AppUserResponse> createUser(@RequestBody AppUserRequest appUserRequest) {
+    return new ResponseEntity<>(appUserServiceImpl.createUser(appUserRequest), HttpStatus.CREATED);
+  }
+}
