f915e99438e4460af1af69a3d05cc464ab388599
Andre <andre_oliiveira97@hotmail.com>
2022-07-19 03:05:10 -0300

    add new endpoint

--- src/main/java/com/example/authjwt/controllers/AppUserController.java
+++ src/main/java/com/example/authjwt/controllers/AppUserController.java
@@ -1,9 +1,12 @@
 package com.example.authjwt.controllers;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.authjwt.dtos.AppUserRequest;
 import com.example.authjwt.dtos.AppUserResponse;
+import com.example.authjwt.dtos.LoginRequest;
 import com.example.authjwt.services.AppUserServiceImpl;
 
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class AppUserController {
 
@@ -25,5 +29,15 @@ public class AppUserController {
   @PostMapping
   public ResponseEntity<AppUserResponse> createUser(@RequestBody @Valid AppUserRequest appUserRequest) {
     return new ResponseEntity<>(appUserServiceImpl.createUser(appUserRequest), HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<AppUserResponse>> listUsers() {
+    return new ResponseEntity<>(appUserServiceImpl.listUsers(), HttpStatus.OK);
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
+    return new ResponseEntity<>("Login bem sucedido", HttpStatus.ACCEPTED);
   }
 }
