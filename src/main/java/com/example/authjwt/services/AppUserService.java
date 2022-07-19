4023254eb86e8df8553fd37c04d05cd5be45a592
Andre <andre_oliiveira97@hotmail.com>
2022-07-19 03:04:54 -0300

    add userDetailsService method

--- src/main/java/com/example/authjwt/services/AppUserService.java
+++ src/main/java/com/example/authjwt/services/AppUserService.java
@@ -1,9 +1,18 @@
 package com.example.authjwt.services;
 
+import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.example.authjwt.dtos.AppUserRequest;
 import com.example.authjwt.dtos.AppUserResponse;
 
-public interface AppUserService {
+public interface AppUserService extends UserDetailsService {
+
+  UserDetails loadUserByUsername(String username);
 
   AppUserResponse createUser(AppUserRequest appUserRequest);
+
+  List<AppUserResponse> listUsers();
 }
--- src/main/java/com/example/authjwt/services/AppUserServiceImpl.java
+++ src/main/java/com/example/authjwt/services/AppUserServiceImpl.java
@@ -1,5 +1,13 @@
 package com.example.authjwt.services;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class AppUserServiceImpl implements AppUserService {
 
   private final AppUserRepository appUserRepository;
+  private final BCryptPasswordEncoder encoder;
 
   @Override
   public AppUserResponse createUser(AppUserRequest appUserRequest) {
@@ -25,9 +34,26 @@ public class AppUserServiceImpl implements AppUserService {
       throw new EmailAlreadyUsedException("O email escolhido ja esta sendo usado!");
     } else {
       AppUser user = new AppUser(appUserRequest.getNome(), appUserRequest.getEmail(), appUserRequest.getPassword());
+      user.setPassword(encoder.encode(user.getPassword()));
       user = appUserRepository.save(user);
       return new AppUserResponse(user.getId(), user.getName(), user.getEmail());
     }
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    AppUser user = appUserRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("Usuario não encontrado"));
+
+    return new User(user.getEmail(), user.getPassword(), new ArrayList<>());
+  }
+
+  @Override
+  public List<AppUserResponse> listUsers() {
+    return appUserRepository.findAll().stream()
+        .map(user -> new AppUserResponse(user.getId(), user.getName(), user.getEmail()))
+        .collect(Collectors.toList());
+
   }
 
 }
